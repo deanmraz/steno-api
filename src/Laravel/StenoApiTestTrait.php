d7c42d8a4a1cc25acd2ee981a87164e83505653d
Dean Mraz <deanm@runtriz.com>
2015-05-24 23:43:22 -0700

    asserting resource and its attributes

--- src/Laravel/StenoApiTestTrait.php
+++ src/Laravel/StenoApiTestTrait.php
@@ -4,12 +4,18 @@ use DMraz\StenoApi\DocumentParser;
 
 trait StenoApiTestTrait
 {
+  protected $document;
+
   protected function getDocument()
   {
-    $file = base_path($this->file);
-    $document = new DocumentParser;
-    $document->load($file);
-    return $document;
+    if(empty($this->document))
+    {
+      $file = base_path($this->file);
+      $document = new DocumentParser;
+      $document->load($file);
+      $this->document = $document;
+    }
+    return $this->document;
   }
 
   protected function tryAllOperations()
@@ -28,6 +34,25 @@ trait StenoApiTestTrait
     $method = $operation['method'];
     $crawler = $this->client->request($method, $uri);
     $this->assertTrue($this->client->getResponse()->isOk(), "$method $uri is okay");
+    $this->checkContent($this->client->getResponse()->getContent());
+  }
+
+  protected function checkContent($content)
+  {
+    $result = json_decode($content, true);
+    $document = $this->getDocument();
+
+    //check resource payload
+    $name = $document->getResource()['name'];
+    $name = str_plural(strtolower($name));
+    $this->assertArrayHasKey($name, $result);
+
+    //check attributes
+    $attributes = $document->getResource()['attributes'];
+    foreach($attributes as $attribute)
+    {
+      $this->assertArrayHasKey($attribute['name'], $result[$name][0], "Checking $name for attribute {$attribute['name']}");
+    }
   }
 
 }
