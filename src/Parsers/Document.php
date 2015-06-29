8f1fad44171eae3f7677300b88afb4283ec58421
Dean Mraz <deanm@runtriz.com>
2015-06-28 19:37:24 -0700

    section segmentation and testing

--- src/Parsers/Document.php
+++ src/Parsers/Document.php
@@ -1,15 +1,22 @@
 <?php
 namespace DMraz\StenoApi\Parsers;
 
+use Illuminate\Support\Collection;
+
 class Document
 {
+  protected $lines;
+  protected $sections;
+
   public function parse($document)
   {
     $document = $this->cleanup($document);
 
     # get line collection
-    $lines = $this->segmentLines($document);
+    $this->lines = $lines = $this->segmentLines($document);
 
+    # section the lines
+    $this->sections = $sections = $this->segmentSections($lines);
   }
 
   protected function cleanup($document)
@@ -25,13 +32,110 @@ class Document
 
   protected function segmentLines($document)
   {
-    $collection = new LineCollection;
+    $collection = new Collection;
     $lines = explode("\n", $document);
     foreach($lines as $key => $line)
     {
       $collection->push(new Line($line, $key));
     }
     return $collection;
+  }
+
+  protected function isAssociativeArray($array)
+  {
+    return array_keys($array) !== range(0, count($array) - 1);
+  }
+
+  protected function segmentSections(Collection $lineCollection)
+  {
+    $sections = new Collection;
+    $lines = $lineCollection->all();
+    $lastSection = null;
+    $lastListAttribute = null;
+    foreach($lines as $key => $line)
+    {
+      $method = "segment".studly_case($line->type)."Line";
+      $result = $this->$method($line, $lastSection, $lastListAttribute);
+
+      //set section
+      if(is_object($result)) {
+        $sections->push($result);
+        $lastSection = $sections->last();
+      // set last list attribute
+      } else if(!empty($result)) {
+        $lastListAttribute = $result;
+      }
+    }
+    return $sections;
+  }
+
+  /**
+   * @param $line Line
+   * @return Section
+   */
+  protected function segmentParentLine($line)
+  {
+    $section = new Section;
+    $section->depth = strlen($line->leads);
+    $section->key = $line->key;
+    $section->value = $line->value;
+    $section->title = $line->text;
+    return $section;
+  }
+
+  /**
+   * @param $line
+   * @param $section Section
+   */
+  protected function segmentTextLine($line, $section)
+  {
+    $section->setDescription($line->text);
+  }
+
+  /**
+   * @param $line
+   * @param $section Section
+   */
+  protected function segmentListLine($line, $section)
+  {
+    $section->addAttributeList($line->key);
+    return $line->key;
+  }
+
+  /**
+   * @param $line
+   * @param $section Section
+   */
+  protected function segmentListItemLine($line, $section, $attribute)
+  {
+    $section->addAttributeListItem($attribute, $line->text);
+  }
+
+  /**
+   * @param $line
+   * @param $section Section
+   */
+  protected function segmentListKeyValueLine($line, $section, $attribute)
+  {
+    $section->addAttributeListKeyValue($line->key, $line->value, $attribute);
+  }
+
+  /**
+   * @param $line
+   * @param $section Section
+   */
+  protected function segmentKeyValueLine($line, $section)
+  {
+    $section->addAttributeKeyValue($line->key, $line->value);
+  }
+
+  /**
+   * @param $line
+   * @param $section Section
+   */
+  protected function segmentNoneLine($line, $section)
+  {
+
   }
 
 }
--- src/Tests/ParserDocumentTest.php
+++ src/Tests/ParserDocumentTest.php
@@ -33,7 +33,7 @@ class ParserDocumentTest extends \PHPUnit_Framework_TestCase
     $line3 = "list:";
     $line4 = "+ item value";
     $line5 = "key: value";
-    $line6 = "list:";
+    $line6 = "lists:";
     $line7 = "+ key: value";
     $line8 = "### Key: value";
     $line9 = "description";
@@ -43,32 +43,28 @@ class ParserDocumentTest extends \PHPUnit_Framework_TestCase
 
     $lines = $this->callMethod('segmentLines', [$string]);
 
+    //test types
+    $this->assertEquals('parent', $lines->get(0)->type);
+    $this->assertEquals('text', $lines->get(1)->type);
+    $this->assertEquals('parent', $lines->get(2)->type);
+    $this->assertEquals('list', $lines->get(3)->type);
+    $this->assertEquals('list_item', $lines->get(4)->type);
+    $this->assertEquals('key_value', $lines->get(5)->type);
+    $this->assertEquals('list', $lines->get(6)->type);
+    $this->assertEquals('list_key_value', $lines->get(7)->type);
+    $this->assertEquals('parent', $lines->get(8)->type);
+    $this->assertEquals('text', $lines->get(9)->type);
+    $this->assertEquals('key_value', $lines->get(10)->type);
+
     //tests lines
     $this->assertEquals($line0,$lines->get(0)->original);
     $this->assertEquals($line1,$lines->get(1)->original);
     $this->assertEquals($line2,$lines->get(2)->original);
-
     $this->assertEquals($line3,$lines->get(3)->original);
-
-    //test parents
-    $this->assertTrue($lines->get(0)->parent);
-    $this->assertTrue($lines->get(2)->parent);
-    $this->assertTrue($lines->get(8)->parent);
-
-    //not parents
-    $this->assertFalse($lines->get(1)->parent);
-
-    //test list items
-    $this->assertTrue($lines->get(4)->list_item);
-    $this->assertTrue($lines->get(7)->list_item);
-
-    //not list item
-    $this->assertFalse($lines->get(0)->list_item);
-    $this->assertFalse($lines->get(1)->list_item);
 
     //test key
     $this->assertEquals('list', $lines->get(3)->key);
-    $this->assertEquals('list', $lines->get(6)->key);
+    $this->assertEquals('lists', $lines->get(6)->key);
     $this->assertEquals('key', $lines->get(5)->key);
     $this->assertEquals('key', $lines->get(7)->key);
     $this->assertEquals('Key', $lines->get(8)->key);
@@ -90,5 +86,46 @@ class ParserDocumentTest extends \PHPUnit_Framework_TestCase
     $this->assertFalse($lines->get(6)->text);
     $this->assertFalse($lines->get(8)->text);
     $this->assertFalse($lines->get(10)->text);
+
+  }
+
+  public function testSections()
+  {
+    $line0 = "# Header";
+    $line1 = "Description";
+    $line2 = "## Sub Header";
+    $line3 = "list:";
+    $line4 = "+ item value";
+    $line5 = "attribute: attribute_value";
+    $line6 = "lists:";
+    $line7 = "+ key: value";
+    $line8 = "### Key: value";
+    $line9 = "description";
+    $line10 = "attribute: value";
+
+    $string = "$line0\n$line1\n$line2\n$line3\n$line4\n$line5\n$line6\n$line7\n$line8\n$line9\n$line10\n";
+
+    $lines = $this->callMethod('segmentLines', [$string]);
+
+    $sections = $this->callMethod('segmentSections', [$lines]);
+
+    //section 1
+    $this->assertEquals('Header', $sections->get(0)->title);
+    $this->assertEquals('Description', $sections->get(0)->getDescription());
+
+    //section 2
+    $this->assertEquals('Sub Header', $sections->get(1)->title);
+    $this->assertNull($sections->get(1)->getDescription());
+    $list = $sections->get(1)->get('list');
+    $this->assertTrue(is_array($list));
+    $this->assertEquals('item value',$list[0]);
+    $this->assertEquals('attribute_value', $sections->get(1)->get('attribute'));
+    $this->assertEquals('value',$sections->get(1)->get('lists.key'));
+
+    //section 3
+    $this->assertEquals('Key', $sections->get(2)->key);
+    $this->assertEquals('value', $sections->get(2)->value);
+    $this->assertEquals('description', $sections->get(2)->getDescription());
+    $this->assertEquals('value', $sections->get(2)->get('attribute'));
   }
 }
